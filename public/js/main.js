44f73eed04bacce068572ed3e66f505aa072afdc
Md Wasim Ansari <119097409+md-wasim-123@users.noreply.github.com>
2023-08-20 00:19:25 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,33 @@
 let output = document.getElementById('output')
+function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+         month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+         hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }   
+    var dateTime = year+'/'+month+'/'+day+'  ||  '+hour+':'+minute+':'+second;   
+     return dateTime;
+}
 
-let date = new Date()
-let getDate = date.getDate()
-let getM = date.getMonth()
-let gety = date.getFullYear()
-let times = date.getSeconds()
-let timem = date.getMinutes()
-let timeh = date.getHours()
-
-output.innerHTML = getDate + '/' + getM + '/' + gety + '  ' + ' Time ' + times + '-' + timem + '-' + timeh
- 
+// example usage: realtime clock
+setInterval(function(){
+    currentTime = getDateTime();
+    output.innerHTML = currentTime;
+}, 1000);
