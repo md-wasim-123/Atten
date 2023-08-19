aebb8dc8ccf8b9c83ef96584b29dd5d5183b3f68
Md Wasim Ansari <119097409+md-wasim-123@users.noreply.github.com>
2023-08-20 00:29:12 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,35 @@
 let output = document.getElementById('output')
 function getDateTime() {
-    var now     = new Date(); 
-    var year    = now.getFullYear();
-    var month   = now.getMonth()+1; 
-    var day     = now.getDate();
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds(); 
-    if(month.toString().length == 1) {
-         month = '0'+month;
+    let time = new Date().toLocaleTimeString();
+    let now = new Date()
+    console.log(time)
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    if (month.toString().length == 1) {
+        month = '0' + month;
     }
-    if(day.toString().length == 1) {
-         day = '0'+day;
-    }   
-    if(hour.toString().length == 1) {
-         hour = '0'+hour;
+    if (day.toString().length == 1) {
+        day = '0' + day;
     }
-    if(minute.toString().length == 1) {
-         minute = '0'+minute;
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
     }
-    if(second.toString().length == 1) {
-         second = '0'+second;
-    }   
-    var dateTime = year+'/'+month+'/'+day+'  ||  '+hour+':'+minute+':'+second;   
-     return dateTime;
+    if (minute.toString().length == 1) {
+        minute = '0' + minute;
+    }
+    if (second.toString().length == 1) {
+        second = '0' + second;
+    }
+    var dateTime = year + '/' + month + '/' + day + '  ||  '+ time
+    return dateTime;
 }
 
 // example usage: realtime clock
-setInterval(function(){
+setInterval(function () {
     currentTime = getDateTime();
     output.innerHTML = currentTime;
 }, 1000);
